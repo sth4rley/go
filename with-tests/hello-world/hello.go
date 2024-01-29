df9f7c58b42ce88b1617f7d018d149de2f527a89
Stharley <sstharley@gmail.com>
2024-01-29 20:46:46 -0300

    updates

--- with-tests/hello-world/hello.go
+++ with-tests/hello-world/hello.go
@@ -2,15 +2,31 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+    spanish = "Spanish"
+    french = "French"
 
-func Hello(name string) string {
+    englishHelloPrefix = "Hello, "
+    spanishHelloPrefix = "Hola, "
+    frenchHelloPrefix = "Bonjour, "
+)
+
+func Hello(name string, lang string) string {
     if name ==  "" {
         name = "World"
     }
-    return englishHelloPrefix + name
+    return greetingPrefix(lang) + name
+}
+
+func greetingPrefix(lang string) (prefix string) {
+    switch lang {
+        case spanish: prefix = spanishHelloPrefix
+        case french: prefix = frenchHelloPrefix
+        default: prefix = englishHelloPrefix
+    }
+    return
 }
 
 func main() {
-    fmt.Println(Hello("World!"));
+    fmt.Println(Hello("World!", ""));
 }
